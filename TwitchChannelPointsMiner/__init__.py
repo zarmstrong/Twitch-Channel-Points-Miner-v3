e66da708a2ddbe52013d0e83bf789f9fa0865548
zarmstrong <git@traffid.com>
2026-07-14 16:27:12 -0600

    chore: bootstrap Release Please for 3.0.0
    - reset the tracked version baseline to 2.0.5
    - configure Release Please to create the 3.0.0 release
    - upgrade badge workflows to actions/checkout v6

--- TwitchChannelPointsMiner/__init__.py
+++ TwitchChannelPointsMiner/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "3.0.0"  # x-release-please-version
+__version__ = "2.0.5"  # x-release-please-version
 
 from .TwitchChannelPointsMiner import TwitchChannelPointsMiner
 
